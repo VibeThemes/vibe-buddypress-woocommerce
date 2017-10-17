2300faae992751cb7ed5ae7f59e474acd86f72e7
HK <hitesh@99fusion.com>
2017-10-17 19:29:11 +0530

    Translation added

--- loader.php
+++ loader.php
@@ -16,5 +16,21 @@ Domain Path: /languages/
 
 if ( !defined( 'ABSPATH' ) ) exit;
 
-include_once('class.init.php');
-include_once('class.settings.php');
+include_once('includes/class.init.php');
+include_once('includes/class.settings.php');
+
+add_action('plugins_loaded','vibe_buddypress_woocommerce_translations');
+function vibe_buddypress_woocommerce_translations(){
+
+    $locale = apply_filters("plugin_locale", get_locale(), 'vbc');
+    $lang_dir = dirname( __FILE__ ) . '/languages/';
+    $mofile        = sprintf( '%1$s-%2$s.mo', 'vbc', $locale );
+    $mofile_local  = $lang_dir . $mofile;
+    $mofile_global = WP_LANG_DIR . '/plugins/' . $mofile;
+
+    if ( file_exists( $mofile_global ) ) {
+        load_textdomain( 'vbc', $mofile_global );
+    } else {
+        load_textdomain( 'vbc', $mofile_local );
+    }  
+}
