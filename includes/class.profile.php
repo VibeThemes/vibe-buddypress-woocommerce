f56c19f0a407eb81d97798969a5a43b8984fb3b1
Logan <madhvendras84@gmail.com>
2019-03-07 17:14:17 +0530

    WP_Query for products

--- includes/class.profile.php
+++ includes/class.profile.php
@@ -30,7 +30,7 @@ class Vibe_BP_Profile_Shop{
 			$access = apply_filters('vibe_bp_shop_access',bp_is_my_profile());
 
 		$main_slug = _x( 'shop','my products sub nav slug in profile', 'vbc' );
-		$main_link = trailingslashit( bp_loggedin_user_domain() . $main_slug );
+		$this->main_link = trailingslashit( bp_loggedin_user_domain() . $main_slug );
 
         bp_core_new_nav_item( array( 
             'name' => _x( 'Shop','My products sub nav item in profile','vbc' ), 
@@ -45,7 +45,7 @@ class Vibe_BP_Profile_Shop{
 			'name' 		      => _x( 'Dashboard','My products sub nav item in profile','vbc' ),
 			'slug' 		  	  => _x( 'dashboard','my products sub nav slug in profile', 'vbc' ),
 			'parent_slug'     => $main_slug,
-			'parent_url'      => $main_link,
+			'parent_url'      => $this->main_link,
 			'screen_function' => array($this,'vbc_shop'),
 			'position' 	  		=> 30,
 			'user_has_access' => $access // Only the logged in user can access this on his/her profile
@@ -55,7 +55,7 @@ class Vibe_BP_Profile_Shop{
 			'name' 		      => _x( 'My Products','My products sub nav item in profile','vbc' ),
 			'slug' 		  	  => _x( 'my-products','my products sub nav slug in profile', 'vbc' ),
 			'parent_slug'     => $main_slug,
-			'parent_url'      => $main_link,
+			'parent_url'      => $this->main_link,
 			'screen_function' => array($this,'myproducts'),
 			'position' 	  		=> 30,
 			'user_has_access' => $access // Only the logged in user can access this on his/her profile
@@ -65,7 +65,7 @@ class Vibe_BP_Profile_Shop{
 			'name' 		      => _x( 'Add/Edit Product','My products sub nav item in profile','vbc' ),
 			'slug' 		  	  => _x( 'add-edit-product','my products sub nav slug in profile', 'vbc' ),
 			'parent_slug'     => $main_slug,
-			'parent_url'      => $main_link,
+			'parent_url'      => $this->main_link,
 			'screen_function' => array($this,'add_product'),
 			'position' 	  		=> 30,
 			'user_has_access' => $access // Only the logged in user can access this on his/her profile
@@ -81,21 +81,72 @@ class Vibe_BP_Profile_Shop{
     }
 
     function set_title(){
-    	echo '<h2>Ok</h2>';
+    	echo '<h2>'.__('My Dashboard','vbc').'</h2>';
     }
 
     function set_content(){
-    	echo 'Ye h hai content';
+    	//dashboard graphs
     }
 
 
     function myproducts(){
-    	add_action('bp_template_title',array($this,'set_title'));
-    	add_action('bp_template_content',array($this,'set_content'));
+    	add_action('bp_template_title',array($this,'set_myproducts_title'));
+    	add_action('bp_template_content',array($this,'set_myproducts_content'));
     	bp_core_load_template( apply_filters( 'bp_core_template_plugin', 'members/single/plugins' ) );
     	exit();
     }
 
+    function set_myproducts_title(){
+    	echo '<h2>'.__('My Products','vbc').'</h2>';
+    }
+
+    function set_myproducts_content(){
+
+    	$args = apply_filters('vibe_bp_woo_my_products',array(
+    		'post_type'=>'product',
+    		'post_author'=>bp_displayed_user_id(),
+    		'paged'=>1,
+    		'posts_per_page'=>20,
+    	));
+
+    	$query = new WP_Query($args);
+    	if($query->have_posts()){
+    		echo '<ul class="myproducts">';
+    		while($query->have_posts()){
+    			$query->the_post();
+    			echo '<li>';
+    			echo '<div class="vbc_myproduct"><div class="vbc_product_image">';
+    			the_post_thumbnail(array('size'=>'medium'));
+    			echo '</div><div class="vbc_product_content">';
+    			echo '<h3>'.get_the_title().'</h3>';
+				$product_cats = get_the_terms(get_the_ID(),'product_cat');
+				if(!empty($product_cats)){
+					echo '<div class="product_cats">';
+					foreach($product_cats as $cat){
+						echo '<a href="'.get_term_link($cat).'">'.$cat->name.'</a>';
+					}
+					echo '</div>';
+				}
+    			echo '<p>'.get_the_excerpt().'</p>';
+    			echo '</div></div>';
+    			echo '</li>';
+    		}
+    		echo '</ul>';
+    	}else{
+    		echo '<div class="message"><p>'.sprintf(__('No products found. %s Create your first product %s','vbc'),'<a href="'.$this->main_link.'/'._x( 'add-edit-product','my products sub nav slug in profile', 'vbc' ).'">','</a>').'</p></div>';
+    	}
+
+    	?>
+    	<style>
+    	ul.myproducts {
+    		display: grid;
+    		grid-gap:10px;
+    		grid-template-columns: repeat(auto-fit,minmax(240px,1fr));
+    		font-size: 14px;
+		}
+    	</style>
+    	<?php
+    }
 
     function add_product(){
     	add_action('bp_template_title',array($this,'set_title'));
